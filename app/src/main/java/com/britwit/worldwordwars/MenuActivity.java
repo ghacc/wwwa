8fb64a391a044b8490e56f18d82edf4ea897ac76
emp <acclist@hotmail.com>
2015-07-05 04:05:40 +0300

    MenuActivity set to fullscreen

--- app/src/main/java/com/britwit/worldwordwars/MenuActivity.java
+++ app/src/main/java/com/britwit/worldwordwars/MenuActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
 
 public class MenuActivity extends AppCompatActivity {
@@ -11,7 +13,9 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_menu);
     }
+
 
 }
